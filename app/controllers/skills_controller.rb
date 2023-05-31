4ceb913f14c377e6e54a0a57b4b8c2397c95131d
EmmanuelBahena <mannybahena04@gmail.com>
2023-05-31 14:12:12 -0500

    add new/create controller action

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -3,4 +3,16 @@ class SkillsController < ApplicationController
     @skills = Skill.all
     render :index
   end
+
+  def new
+    @skill = Skill.new
+    render :new
+  end
+  def create
+    @skill = Skill.create(
+      skill_name: params[:skill_name],
+    )
+    render :show
+  end
+
 end
