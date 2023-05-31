985d771903b90fb42c3a4311c9b9a7c43d5a3422
EmmanuelBahena <mannybahena04@gmail.com>
2023-05-31 13:12:05 -0500

    add skills model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_174542) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_180826) do
+  create_table "skills", force: :cascade do |t|
+    t.string "skill_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
