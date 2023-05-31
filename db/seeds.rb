24d1ac2ec310f5bea7bdcc62a615442cd28462aa
Mattias Schade <mattiasschade@gmail.com>
2023-05-31 14:37:31 -0500

    added ffaker and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'ffaker'
+
+50.times do
+  student = Student.new(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, phone_number: FFaker::PhoneNumber.short_phone_number, short_bio: FFaker::Lorem.paragraph, linkedin: "linkedin.com",  twitter: "@#{FFaker::InternetSE.domain_word}", personal_website: FFaker::Internet.http_url, online_resume: "resume.com", github: "github.com", photo: FFaker::Image.url  )
+
+  student.email = "#{student.first_name.downcase}#{student.last_name.downcase}@gmail.com"
+
+  student.save
+end
+
+
