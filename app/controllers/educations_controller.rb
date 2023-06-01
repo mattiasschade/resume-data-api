6aa773e872aa0d58a732199cf989c5ee8c789421
Kimber Ogden <mx.kimber@gmail.com>
2023-05-31 21:20:10 -0500

    Educations and Experiences Update actions

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -20,5 +20,17 @@ class EducationsController < ApplicationController
     )
     render :show
   end
+
+  def update
+    @education = Education.find_by(id: params[:id])
+    @education.update(
+      start_date: params[:start_date] || @education.start_date, 
+      end_date: params[:end_date] || @education.end_date,
+      degree: params[:degree]|| @education.degree,
+      university_name: params[:university_name] || @education.university_name,
+      details: params[:details] || @education.details,
+    )
+    render :show
+  end
 end
 
