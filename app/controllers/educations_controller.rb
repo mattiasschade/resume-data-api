c9b5eb7f5edec7b0c4e6183678360d4d43b88463
Kimber Ogden <mx.kimber@gmail.com>
2023-05-31 21:09:05 -0500

    Educations and Experiences Create, Enable CSRF protection with exception for JSON requests

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -10,5 +10,15 @@ class EducationsController < ApplicationController
     render :show
   end
 
+  def create
+    @education = Education.create(
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      degree: params[:degree],
+      university_name: params[:university_name],
+      details: params[:details],
+    )
+    render :show
+  end
 end
 
--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -2,10 +2,22 @@ class ExperiencesController < ApplicationController
 
   def index
     @experiences = Experience.all
+    render :index
   end 
 
   def show
     @experience = Experience.find_by(id: params[:id])
+    render :show
+  end
+  
+  def create
+    @experience = Experience.create(
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      job_title: params[:job_title],
+      company_name: params[:company_name],
+      details: params[:details],
+    )
     render :show
   end
 end
