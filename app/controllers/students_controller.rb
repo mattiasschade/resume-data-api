2252cf1792f3a457fe4b0a21f715b2457d13c0d5
Mattias Schade <mattiasschade@gmail.com>
2023-06-01 11:47:55 -0500

    changed students routes, students create

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,4 +11,24 @@ class StudentsController < ApplicationController
     render :show
   end
   
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      short_bio: params[:short_bio],
+      linkedin: params[:linkedin],
+      twitter: params
+    )
+    if @student.save
+      render :show
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
-  get "/students" => "students#index"
-  get "/students/:id" => "students#show"
+  # get "/students" => "students#index"
+  # get "/students/:id" => "students#show"
 
+  # post "/students" => "students#create"
+
+  resources :students
   resources :educations
   resources :experiences
   
