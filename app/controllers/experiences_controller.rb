150c161d075c37a707c407869db68333ac8eceb0
Kimber Ogden <mx.kimber@gmail.com>
2023-06-02 13:28:42 -0500

    Added user_id to update/create in experiences controller/jbuilder

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,5 +1,4 @@
 class ExperiencesController < ApplicationController
-
   def index
     @experiences = Experience.all
     render :index
@@ -17,23 +16,27 @@ class ExperiencesController < ApplicationController
       job_title: params[:job_title],
       company_name: params[:company_name],
       details: params[:details],
+      student_id: current_user.id
     )
+    
     if @experience.save
       render :show
     else
-      render json: {errors: @experience.errors.full_messages}, status: :unprocessable_entity
+      render json: { errors: @experience.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def update
     @experience = Experience.find_by(id: params[:id])
-    @experience.start_date = params[:start_date] || @experience.start_date
-    @experience.end_date = params[:end_date] || @experience.end_date
-    @experience.job_title = params[:job_title] || @experience.job_title
-    @experience.company_name = params[:company_name] || @experience.company_name
-    @experience.details = params[:details] || @experience.details
-    @experience.student_id = params[:student_id] || @experience.student_id
-
+    @experience.update(
+      start_date: params[:start_date] || @experience.start_date,
+      end_date: params[:end_date] || @experience.end_date,
+      job_title: params[:job_title] || @experience.job_title,
+      company_name: params[:company_name] || @experience.company_name,
+      details: params[:details] || @experience.details,
+      student_id: params[:student_id] || @experience.student_id
+    )
+    
     @experience.save
     render :show
   end
@@ -41,6 +44,6 @@ class ExperiencesController < ApplicationController
   def destroy
     @experience = Experience.find_by(id: params[:id])
     @experience.destroy
-    render json: { message: "experience destroyed successfully" }
+    render json: { message: "Experience destroyed successfully" }
   end
 end
